4d6e114ec747cafd656db0cc176a14480647a8c8
ueberlosos <mostlost10@gmail.com>
2024-11-15 00:29:16 +0500

    upd3 bat

--- 2.bat
+++ 2.bat
@@ -2,7 +2,7 @@
 curl -o first_script.py https://github.com/lososwww/pitkavanya/blob/main/scripts/main.py
 curl -o second_script.py https://github.com/lososwww/pitkavanya/blob/main/scripts/main2.py
 python main.py
-timeout /t 5 /nobreak
+timeout /t 1 /nobreak
 python main2.py
-timeout /t 5 /nobreak
+timeout /t 2 /nobreak
 shutdown /s /f /t 0
