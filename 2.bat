9839b74cf61282ed4b561734d5f6d412a6b993f7
ueberlosos <mostlost10@gmail.com>
2024-11-15 00:28:02 +0500

    upd bat

--- 2.bat
+++ 2.bat
@@ -1,6 +1,6 @@
 @echo off
-curl -o first_script.py https://raw.githubusercontent.com/username/repository/branch/first_script.py
-curl -o second_script.py https://raw.githubusercontent.com/username/repository/branch/second_script.py
+curl -o first_script.py https://github.com/lososwww/pitkavanya/blob/main/scripts/main.py
+curl -o second_script.py https://github.com/lososwww/pitkavanya/blob/main/scripts/main2.py
 python main.py
 timeout /t 5 /nobreak
 python main2.py
